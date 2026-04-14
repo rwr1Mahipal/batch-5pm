a51126f2cb6252ef403d7d3dfa735a6f40591e62
rwr1Mahipal <rwr1.mahipal.sb@gmail.com>
2026-04-14 18:01:49 +0530

    Add files via upload

---
+++ C/1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    printf("\tHello");
+    printf("\nStudent's");
+
+    int a = 10;
+    char b = 'a';
+    float c = 10.24;
+    bool d = true;
+    double e = 10.255648755;
+
+    printf("\n%d",a);
+    printf("\n%c",b);
+    printf("\n%f",c);
+    printf("\n%d",d);
+    printf("\n%lf",e);
+}
